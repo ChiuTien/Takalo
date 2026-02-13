85991bea53450c70b44a2375b328b3bce2fef8c3
ChiuTien <chiumandresychristian@gmail.com>
2026-02-13 20:44:15 +0300

    J'ai arranger le login et la route

--- app/config/routes.php
+++ app/config/routes.php
@@ -14,17 +14,11 @@ use flight\net\Router;
 $router->group('', function(Router $router) use ($app) {
 
 	$router->get('/', function() use ($app) {
-		$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
+		$app->render('login');
 	});
 
 	$router->get('/hello-world/@name', function($name) {
 		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
-	});
-
-	$router->group('/api', function() use ($router) {
-		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
-		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
-		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
 	});
 	
 }, [ SecurityHeadersMiddleware::class ]);
